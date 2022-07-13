3b29efe3d7956fbf776bbad48393c30ba4671169
iarleymatheus <yarley.matheus@hotmail.com>
2022-07-13 14:44:37 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Container 1</h1>
+      </div>
+      <div>
+        <h1>Container 2</h1>
+      </div>
+      <div>
+        <h1>Container 3</h1>
+      </div>
+      <div>
+        <h1>Container 4</h1>
+      </div>
+      <div>
+        <h1>Container 5</h1>
+      </div>
+      <div>
+        <h1>Container 6</h1>
+      </div>
     </div>
   );
 }
---
+++ src/GlobasStyle.js
@@ -0,0 +1,3 @@
+import styled from "styled-components"
+
+function GlobalStyle = div
