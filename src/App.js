57c17536eab510d9c4a60dcc2e35dfb4a0f7840a
iarleymatheus <yarley.matheus@hotmail.com>
2022-07-14 12:37:19 -0300

    fadin on scroll in react finish

--- src/App.js
+++ src/App.js
@@ -1,45 +1,36 @@
-import { useRef, useEffect , useState } from 'react';
+
+import { useInView } from 'react-intersection-observer';
 
 import './App.css';
 
 function App() {
   
-  const myRef = useRef();
-  const [myElementIsVisible , setMyElementIsVisible] = useState();
-  console.log("myElementIsVisible", myElementIsVisible)
-  useEffect(()=>{
-    let options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.2
-    }
-    const obeserver = new IntersectionObserver((entries)=>{
-    const entry = entries[0];
-    setMyElementIsVisible(entry.isIntersecting)
-  },options)
-  obeserver.observe(myRef.current)
+  
 
-  },[])
+  const { ref: myRef, inView:visiblebox1} = useInView();
+  const { ref: box2, inView: visiblebox2} = useInView();
 
   
   
   return (
     <div className="App">
-      <div className='box1'>
+      <div ref={myRef} className={`box1 fade-in-section ${visiblebox1  ? 'is-visible' : ''}`}>
         <h1 >Container 1</h1>
+        <p>{visiblebox1 ? "Yes" : "No"}</p>
       </div>
-      <div className='box2'>
+      <div ref={box2} className={`box2 fade-in-section ${visiblebox2 ? 'is-visible' : ''}`}>
         <h1>Container 2</h1>
+        <p>{visiblebox2 ? "Yes" : "No"}</p>
       </div>
       <div className='box3'>
         <h1>Container 3</h1>
       </div>
-      <div ref={myRef} className={`box4 fade-in-section ${myElementIsVisible ? 'is-visible' : ''}`}>
+      <div className='box4'>
         <h1>Container 4</h1>
-        <p>{myElementIsVisible ? "Yes" : "No"}</p>
+        <p> grrfgf</p>
       </div>
-      <div ref= {myRef} className={`box5 fade-in-section ${myElementIsVisible ? 'is-visible' : ''}`}>
-        <h1>{myElementIsVisible ? "Yes" : "No"}</h1>
+      <div className="box5">
+        <h1>Container 5</h1>
       </div>
     </div>
   );
